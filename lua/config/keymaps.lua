eb43c8db275c6c5c94d99de623b6f796fd14adc3
Jackie Xu <jackiexu1228@gmail.com>
2024-06-29 14:45:43 -0400

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local set = vim.keymap.set
+opt = { noremap = true, silent = true }
 -- Beginning of line
-set("i", "<C-b>", "<ESC>^i",  { buffer = buf, nowait = true })
-set("i", "<C-e>", "<End>",  { buffer = buf, nowait = true })
+set("i", "<C-b>", "<ESC>^i", opt)
+set("i", "<C-e>", "<End>", opt)
 
+set("n", "<leader>sr", ":%s/<C-c><C-w>//g<Left><Left>", { desc = "search and replace" })
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,18 +11,14 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.coding.codeium" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.editor.navic" },
     -- { import = "lazyvim.plugins.extras.editor.leap" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.ruby" },
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    -- { import = "lazyvim.plugins.extras.lang.java" },
-    { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
